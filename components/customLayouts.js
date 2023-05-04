1dbe6f7173c6ccad245c317679ba110e9551775c
Gio <dumale.jeruzael.bscs2019@gmail.com>
2023-05-05 05:50:27 +0800

    up

--- components/customLayouts.js
+++ components/customLayouts.js
@@ -1,4 +1,5 @@
 import styles from 'imp/styles/Customs.module.css'
+import intro from 'imp/styles/Intro.module.css'
 
 
 export const MainCont = ({children}) => {
@@ -23,12 +24,51 @@ export const Clouds = ({children, ids}) => {
     );
 };
 
-export const Logo = ({children}) => {
+export const Logo = ({children, c}) => {
 
     return(
         <>
-            <div class={styles.mainLogo}>
+            <div class={c}>
                 {children}
+            </div>
+        </>
+    );
+};
+
+export const CloudsCont = ({children}) => {
+
+    return(
+        <>
+            <div >
+                {children}
+            </div>
+        </>
+    );
+};
+
+export const Intro = ({children, ids, imgids}) => {
+    
+    
+    return(
+        <>
+            <div class={intro.intCont}>
+                <div class={intro.sec}>
+                    <div class={intro.textStyle} id={intro.title}>
+
+                    </div>
+                    <div class={intro.textStyle} id={ids}>
+                        <h1>HALE-BOPP Brings Closure to:</h1>
+                        <p>
+                        Whether Hale-Bopp has a "companion" or not is irrelevant from our perspective. However, its arrival is joyously very significant to us at "Heaven's Gate." The joy is that our Older Member in the Evolutionary Level Above Human (the "Kingdom of Heaven") has made it clear to us that Hale-Bopp's approach is the "marker" we've been waiting for -- the time for the arrival of the spacecraft from the Level Above Human to take us home to "Their World" -- in the literal Heavens. Our 22 years of classroom here on planet Earth is finally coming to conclusion -- "graduation" from the Human Evolutionary Level. We are happily prepared to leave "this world" and go with Ti's crew.
+If you study the material on this website you will hopefully understand our joy and what our purpose here on Earth has been. You may even find your "boarding pass" to leave with us during this brief "window."
+We are so very thankful that we have been recipients of this opportunity to prepare for membership in Their Kingdom, and to experience Their boundless Caring and Nurturing.
+                        </p>
+                    </div>
+
+                </div>
+                <div class={intro.sec} id={imgids}>
+
+                </div>
             </div>
         </>
     );
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,66 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from 'imp/styles/Home.module.css'
 import CusStyles from 'imp/styles/Customs.module.css'
-import {MainCont, Clouds, Logo} from 'components/customLayouts.js'
+import intro from 'imp/styles/Intro.module.css'
+import {MainCont, Clouds, Logo, Intro} from 'components/customLayouts.js'
+import { useCallback, useEffect, useState } from 'react'
+
 
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [leftCloud, setLeftCloud] = useState(CusStyles.leftClouds);
+  const [leftBottomCloud, setLeftBottomCloud] = useState(CusStyles.leftBottomClouds);
+  const [rightCloud, setRightCloud] = useState(CusStyles.rightClouds);
+  const [rightBottomCloud, setRightBottomCloud] = useState(CusStyles.rightBottomClouds);
+  const [marqClouds, setMarqClouds] = useState(CusStyles.marqueeClouds);
+  const [logoSize, setLogoSize] = useState({
+    w: 1920,
+    h: 995,
+  });
+  const [mainLogoSize, setMainLogoSize] = useState(CusStyles.mainLogo);
+  const [introCont, setIntroCont] = useState(intro.contentHide);
+  const [introImg, setIntroImg] = useState(intro.introImgHide);
+
+  const [scrollY, setScrollY] = useState(0);
+  const onScroll = useCallback(
+    event => {
+      const {pageYOffset, scrollY} = window;
+      if(scrollY >= 50){        
+        setLeftCloud(CusStyles.leftCloudsHide);
+        setLeftBottomCloud(CusStyles.leftBottomCloudsHide);
+        setRightCloud(CusStyles.rightCloudsHide);
+        setRightBottomCloud(CusStyles.rightBottomCloudsHide);
+        setMarqClouds(CusStyles.marqueeCloudsHide);
+        
+        setMainLogoSize(CusStyles.mainLogoResize);
+        console.log("yOffset", pageYOffset, "Scrolly", scrollY);
+        setScrollY(window.pageYOffset);
+        //setIntroCont(intro.contentShow);
+        if(scrollY >= 250){
+          setIntroCont(intro.contentShow);
+          if(scrollY >= 300){
+            setIntroImg(intro.introImg);
+          }
+        }
+      }else{
+        console.log("ScrollY");
+      }      
+      
+    }
+  );
+
+  useEffect(
+    ()=>{
+      window.addEventListener("scroll", onScroll, {passive: true});
+      return () => {
+        window.removeEventListener("scroll", onScroll, {passive: true});
+      }
+    }, []
+  );
+
   return (
     <>
       <Head>
@@ -19,40 +72,63 @@ export default function Home() {
         <link rel="icon" href="/images/haleBoppIcon3.png" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <MainCont>
-          <Clouds ids={CusStyles.leftClouds}><Image
-            src="/images/realClouds-left.png"
-            height={712}
+        <MainCont>       
+          <Clouds >
+          <Image
+            src="/images/starts.png"
+            height={995}
+            width={1945}
+            alt='stars'
+            priority
+          />
+          </Clouds>
+          <Clouds ids={marqClouds}>
+          <Image
+            src="/images/marqueeClouds.png"
+            height={995}
             width={1416}
             alt='leftBigCloud'
-          /></Clouds>
-          <Clouds ids={CusStyles.leftBottomClouds}><Image
+            priority
+          />
+          </Clouds>   
+          <Clouds ids={leftBottomCloud}><Image
             src="/images/realClouds-leftBottom.png"
             height={995}
             width={1416}
             alt='leftBigCloud'
+            priority
           /></Clouds>
-          <Clouds ids={CusStyles.rightClouds}><Image
+          <Clouds ids={rightCloud}><Image
             src={"/images/realClouds-right.png"}
             height={712}
             width={1920}            
             alt='rightBigCloud'
+            priority
           /></Clouds>
-          <Logo>
-          <Image
+          <Logo c={mainLogoSize}>
+          {/*<Image
             src={"/images/logoSample.png"}
-            height={995}
-            width={1920}            
+            height={logoSize.h}
+            width={logoSize.w}            
             alt='rightBigCloud'
-          />
+            priority
+          />*/}
           </Logo>
-          <Clouds ids={CusStyles.rightBottomClouds}><Image
+          <Clouds ids={leftCloud}><Image
+            src="/images/realClouds-left.png"
+            height={712}
+            width={1416}
+            alt='leftBigCloud'
+            priority
+          /></Clouds>
+          <Clouds ids={rightBottomCloud}><Image
             src={"/images/realClouds-rightBottom.png"}
             height={995}
             width={1920}            
             alt='rightBigCloud'
+            priority
           /></Clouds>
-
+          <Intro ids={introCont} imgids={introImg} />          
           
         </MainCont>
       </main>
